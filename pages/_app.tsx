b3e545ab38f2da960988af8da1ab90a27ac0d1a8
Maizares, Juan <juan_maizares386@hotmail.com>
2023-05-16 16:44:46 -0300

    cambio de theme activado

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,21 @@
 import type { AppProps } from 'next/app'
 
 // agregamos la configuracion para NextUI y encapsulasmos
-import { NextUIProvider } from '@nextui-org/react';
-import { darkTheme } from '../themes/darktheme';
-
+import { Button, NextUIProvider } from '@nextui-org/react';
+import { darkTheme, ligthTheme } from '../themes/darktheme';
+import { useState } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [currentTheme, setCurrentTheme] = useState(darkTheme); // Inicialmente, se establece el tema oscuro
+
+  // Función para cambiar el tema
+  const toggleTheme = () => {
+    setCurrentTheme(currentTheme === darkTheme ? ligthTheme : darkTheme);
+  };
+
   return (
-    <NextUIProvider theme={ darkTheme }>
+    <NextUIProvider theme={currentTheme}>
+      <Button onPress={toggleTheme}>Cambiar tema</Button>
       <Component {...pageProps} />
     </NextUIProvider>
   )
--- themes/darktheme.ts
+++ themes/darktheme.ts
@@ -1,6 +1,12 @@
 import { createTheme } from "@nextui-org/react"
 
 export const darkTheme = createTheme({
+  type: 'dark',
+  theme: {
+    colors: {} // override dark theme colors
+  }
+});
+export const ligthTheme = createTheme({
   type: 'ligth',
   theme: {
     colors: {} // override dark theme colors
